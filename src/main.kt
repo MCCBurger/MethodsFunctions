cfa818be016cd8d142ad362534f21b18a4d3a4e8
Brian <bburger@students.mchenry.edu>
2020-02-08 11:43:28 -0600

    Added Comments

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,11 @@
 import kotlin.math.*
+
 fun main() {
     //Ask user for choice
     var choice = 0
     //Print menu
-    do{
-    println("=================== AREA CALCULATOR =============")
+    do {
+        println("=================== AREA CALCULATOR =============")
         println("Please choose from the menu below to calculate the area of different shapes")
         println("1 - Area of a Circle")
         println("2 - Area of a Triangle")
@@ -15,55 +16,54 @@ fun main() {
         print("Please enter your choice: ")
         choice = readLine()!!.toInt()
         //Validate and run the function of choice
-        when(choice) {
+        when (choice) {
             1 -> areaCircle()
             2 -> areaTriangle()
             3 -> areaEllipse()
             4 -> areaSquare()
-            !in 1..4 -> if(choice == 9) println("End of Line") else println("INVALID CHOICE")
+            !in 1..4 -> if (choice == 9) println("End of Line") else println("INVALID CHOICE")
         }
-
-
-    }while(choice !=9)
-
-
+    } while (choice != 9)
 }
+
 //Calculate area of a circle
-fun areaCircle(){
-     var radius =0.0
+fun areaCircle() {
+    var radius = 0.0
     println("============== AREA OF CIRCLE ================")
     print("Please input a radius: ")
     radius = readLine()!!.toDouble()
-    println("The area of a circle with a radius of $radius is: ${PI*radius.pow(2)}")
+    println("The area of a circle with a radius of $radius is: ${PI * radius.pow(2)}")
 }
+
 //Calculate area of a triangle
-fun areaTriangle(){
-    var base =0.0
+fun areaTriangle() {
+    var base = 0.0
     var height = 0.0
     println("============== AREA OF TRIANGLE ================")
     print("Please input the Base: ")
     base = readLine()!!.toDouble()
     print("Please input the Height: ")
     height = readLine()!!.toDouble()
-    println("The area of a triangle with a base of: $base and a Height of: $height is: ${.5*base*height}")
+    println("The area of a triangle with a base of: $base and a Height of: $height is: ${.5 * base * height}")
 }
+
 //Calculate area of an ellipse
-fun areaEllipse(){
-    var a =0.0
-    var b =0.0
+fun areaEllipse() {
+    var a = 0.0
+    var b = 0.0
     println("============== AREA OF AN ELLIPSE ================")
     print("Please input side A: ")
     a = readLine()!!.toDouble()
     print("Please input side B: ")
     b = readLine()!!.toDouble()
-    println("The area of a an ellipse with side A: $a and side B: $b is: ${PI*a*b}")
+    println("The area of a an ellipse with side A: $a and side B: $b is: ${PI * a * b}")
 }
+
 //calculate area of a square
-fun areaSquare(){
-    var a =0.0
+fun areaSquare() {
+    var a = 0.0
     println("============== AREA OF A SQUARE ================")
     print("Please input the length of the side: ")
     a = readLine()!!.toDouble()
     println("The area of a square with the side length of: $a is: ${a.pow(2)}")
-
 }
